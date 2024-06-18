ad5a66b3af6b9d108cc61b2015d1a8b5d5d0c659
Nathan Feliphe <nathanfcrocha@gmail.com>
2024-06-18 16:21:25 -0300

    create class formaPagamento

---
+++ ecommerce/src/main/java/br/com/nathanflp/ecommerce/models/formaPagamento.java
@@ -0,0 +1,23 @@
+package br.com.nathanflp.ecommerce.models;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name="formaPagamento")
+@Data
+@NoArgsConstructor
+@SequenceGenerator(name="seq_formaPagamento", sequenceName = "seq_formaPagamento", allocationSize = 1, initialValue = 1)
+public class formaPagamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seq_formaPagamento")
+    private Long id;
+
+    private String descricao;
+
+}
