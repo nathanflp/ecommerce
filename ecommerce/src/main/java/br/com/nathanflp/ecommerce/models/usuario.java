992cf924ca58ba24244f0ab4879b12d8cfeb1cb9
Nathan Feliphe <nathanfcrocha@gmail.com>
2024-06-18 16:21:32 -0300

    create class usuario

---
+++ ecommerce/src/main/java/br/com/nathanflp/ecommerce/models/usuario.java
@@ -0,0 +1,81 @@
+package br.com.nathanflp.ecommerce.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@SequenceGenerator(name="seq_usuario", sequenceName = "seq_usuario", allocationSize = 1, initialValue = 1)
+@Table(name="usuario")
+@Data
+@NoArgsConstructor
+
+public class usuario implements UserDetails {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seq_usuario")
+    private Long id;
+
+    private String login;
+
+    private String senha;
+
+    @Temporal(TemporalType.DATE)
+    private Date data_atua_senha;
+
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "usuarios_acesso", uniqueConstraints = @UniqueConstraint (columnNames = {"usuario_id", "acesso_id"} ,
+    name = "unique_acesso_user"),
+
+    joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id", table = "usuario",
+        unique = false, foreignKey = @ForeignKey(name = "usuario_fk", value = ConstraintMode.CONSTRAINT)),
+
+    inverseJoinColumns = @JoinColumn(name = "acesso_id", unique = false, referencedColumnName = "id", table = "acesso",
+        foreignKey = @ForeignKey(name = "acesso_fk", value = ConstraintMode.CONSTRAINT)))
+
+    List<acessos> acessos;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.acessos;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
