f7462f031fbaa75767129b5788d00ddeb2518ea1
Nathan Feliphe <nathanfcrocha@gmail.com>
2024-06-18 11:48:03 -0300

    create abstract class pessoa

---
+++ ecommerce/src/main/java/br/com/nathanflp/ecommerce/domain/pessoa.java
@@ -0,0 +1,38 @@
+package br.com.nathanflp.ecommerce.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@SequenceGenerator(name="seq_pessoa", sequenceName = "seq_pessoa", allocationSize = 1, initialValue = 1)
+@Table(name="pessoa")
+@Data
+@NoArgsConstructor
+
+public abstract class pessoa implements Serializable {
+
+    private static final long serialVersionUID= 1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_pessoa")
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    @OneToMany(mappedBy = "pessoa", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    List<endereco> enderecos = new ArrayList<endereco>();
+
+}
