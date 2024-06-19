a3dae088fc33a158c33d0eaf34f667e95bfd93d7
Nathan Feliphe <nathanfcrocha@gmail.com>
2024-06-19 12:08:23 -0300

    create class cupomDesc

---
+++ ecommerce/src/main/java/br/com/nathanflp/ecommerce/models/cupDesc.java
@@ -0,0 +1,32 @@
+package br.com.nathanflp.ecommerce.models;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name="cupDesc")
+@Data
+@NoArgsConstructor
+@SequenceGenerator(name="seq_cup_desc", sequenceName = "seq_cup_desc", allocationSize = 1, initialValue = 1)
+
+public class cupDesc {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_cup_desc")
+    private Long id;
+
+    private String codDesc;
+
+    private BigDecimal valorRealDesc;
+
+    private BigDecimal valorPorcentDesc;
+
+    @Temporal(TemporalType.DATE)
+    private Date dataValidadeCupom;
+
+}
