b53dd7d2c344919c890318a92328008e348adb26
Nathan Feliphe <nathanfcrocha@gmail.com>
2024-06-20 18:42:35 -0300

    create class avaliacaoProduto

---
+++ ecommerce/src/main/java/br/com/nathanflp/ecommerce/models/avaliacaoProduto.java
@@ -0,0 +1,30 @@
+package br.com.nathanflp.ecommerce.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name="avaliacao_produto")
+@Data
+@NoArgsConstructor
+@SequenceGenerator(name="seq_avaliacao_produto", sequenceName = "seq_avaliacao_produto", allocationSize = 1, initialValue = 1)
+public class avaliacaoProduto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_avaliacao_produto")
+    private Long id;
+
+    private String descricao;
+
+    private Integer nota;
+
+    @ManyToOne(targetEntity = pessoa.class)
+    @JoinColumn(name="pessoa_id",nullable = false,foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+    private pessoa pessoa;
+
+    @ManyToOne
+    @JoinColumn(name="produto_id",nullable = false,foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "produto_fk"))
+    private produto produto;
+}
