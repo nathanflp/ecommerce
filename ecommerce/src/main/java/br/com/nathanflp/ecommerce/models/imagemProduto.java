a2d8e9e3ffdcf764718ce6d1131f3805e8550ab5
Nathan Feliphe <nathanfcrocha@gmail.com>
2024-06-21 16:11:58 -0300

    change class name

--- ecommerce/src/main/java/br/com/nathanflp/ecommerce/models/imagemProduto.java
+++ ecommerce/src/main/java/br/com/nathanflp/ecommerce/models/imagemProduto.java
@@ -9,16 +9,16 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @SequenceGenerator(name="seq_imagem_produto", sequenceName = "seq_imagem_produto", allocationSize = 1, initialValue = 1)
-public class imageProduto {
+public class imagemProduto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_imagem_produto")
     private Long id;
 
-    @Column(columnDefinition = "text")
+    @Column(columnDefinition = "text", nullable = false)
     private String imagemOriginal;
 
-    @Column(columnDefinition = "text")
+    @Column(columnDefinition = "text", nullable = false)
     private String imagemMiniatura;
 
     @ManyToOne
