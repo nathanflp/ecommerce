804870bd73f9cc15283e75f3e24bea027385760f
Nathan Feliphe <nathanfcrocha@gmail.com>
2024-06-21 16:12:32 -0300

    remove lombok annotation and add getters,setters etc.

--- ecommerce/src/main/java/br/com/nathanflp/ecommerce/models/acessos.java
+++ ecommerce/src/main/java/br/com/nathanflp/ecommerce/models/acessos.java
@@ -2,13 +2,13 @@ package br.com.nathanflp.ecommerce.models;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Objects;
 
 @Entity
 @Table(name="Acessos")
 @Data
-@NoArgsConstructor
 @SequenceGenerator(name="seq_acesso", sequenceName = "seq_acesso", allocationSize = 1, initialValue = 1)
 
 public class acessos implements GrantedAuthority {
@@ -23,5 +23,50 @@ public class acessos implements GrantedAuthority {
     @Override
     public String getAuthority() {
         return null;
+    }
+
+    public acessos() {
+    }
+
+    public acessos(Long id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        acessos acessos = (acessos) o;
+        return Objects.equals(id, acessos.id) && Objects.equals(descricao, acessos.descricao);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, descricao);
+    }
+
+    @Override
+    public String toString() {
+        return "acessos{" +
+                "id=" + id +
+                ", descricao='" + descricao + '\'' +
+                '}';
     }
 }
